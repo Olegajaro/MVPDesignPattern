e44a9075a05b5150cb78b2fe89a52c5443567f1a
Oleg Fedorov <olegajaro94@gmail.com>
2022-09-04 12:17:29 +0700

    Initial Commit

---
+++ MVPDesignPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPDesignPattern
+//
+//  Created by Олег Федоров on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
